59241e55461de124b0cadb65444657ab587056f6
Jack Butler <butlerjz@bc.edu>
2025-10-08 22:55:58 -0400

    Initial Commit

---
+++ Cityscape/CityscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityscapeApp.swift
+//  Cityscape
+//
+//  Created by Jackson Butler on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CityscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cityscape/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cityscape
+//
+//  Created by Jackson Butler on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
